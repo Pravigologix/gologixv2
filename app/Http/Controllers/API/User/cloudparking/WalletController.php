804ba898fdae631bf249265a3c5d7ac60c880488
Naveen B R <naveenramachandru77@gmail.com>
2022-09-27 11:29:57 +0530

    api auth creditail updated

--- app/Http/Controllers/API/User/cloudparking/WalletController.php
+++ app/Http/Controllers/API/User/cloudparking/WalletController.php
@@ -10,52 +10,54 @@ use DB;
 
 class WalletController extends Controller
 {
-    public function updateWallet(Request $request){
-        $WalletModel=new WalletModel();
-        $WalletModel->wal_user_id=$request->wal_user_id;
-        $WalletModel->wal_balance='0';
-        $WalletModel->wal_transaction_id=$request->wal_transaction_id;
-        $WalletModel->credited_amt=$request->credited_amt;
-        $WalletModel->debited_amt=$request->debited_amt;
+    public function addwalletamount(Request $request){
+
+      
+
+        
+        $walletModel=new WalletModel();
+        $walletModel->wal_user_id=$request->input('user_id');
+      
+
+        $walletModel->wal_transaction_id=$request->input('wal_transaction_id');
+        $walletModel->credited_amt=$request->input('credited_amt');
+        $walletModel->debited_amt=$request->input('debited_amt');
+
+        $walletModel->save();
+
+        
+
        
-        if($request->credited_amt){
-            $WalletModel->wal_balance=$WalletModel->wal_balance+$request->credited_amt;
-            return  $WalletModel->wal_balance;
-            $res=$WalletModel->save();
-            return $res;
-        }
 
     }
 
-    public function walletDetails(Request $request)
-    {
-        $data=WalletModel::find($request->input('id'));
-        return response()->json(["wallet details"=>$data],200);
-    }
+    public function getwalletamount(Request $request){
 
-     public function updatingeWallet(Request $request){
+      
+
+        
+        $walletModel=WalletModel()::where('wal_user_id',$request->input('user_id'))->get;
+        $walletModel->wal_user_id=$request->input('user_id');
+      
+
+        $walletModel->wal_transaction_id=$request->input('wal_transaction_id');
+        $walletModel->credited_amt=$request->input('credited_amt');
+        $walletModel->debited_amt=$request->input('debited_amt');
+
+        $walletModel->save();
+
+        
+
+       
+
+    }
 
     
 
-        $data=DB::table('wallet')
-        ->leftjoin('users','wallet.wal_user_id','=','users.id')
-        ->select('users.id','wal_balance','credited_amt','debited_amt')->get();
-        //->update(['wallet.wal_balance','=','wallet.wal_balance','-','$request->debited_amt'])->get();
-        return $data;
-        }
+    
 
 
-/*
 
-       $data=DB::table('wallet')
-      
-       ->where('wallet.wal_user_id','=',$request->id)
-      
-       ->get();
-      
-        return response()->json([$data]);
-    }
-    */
 
      }
 
