926474d0a5dc72e4b6a4ac5059eb7811c984d292
Naveen B R <naveenramachandru77@gmail.com>
2022-09-27 15:36:36 +0530

    qr code updated

---
+++ app/Http/Controllers/API/User/cloudparking/qrcodeController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers\API\User\cloudparking;
+
+use App\Models\BookParkingModel;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+// @@ -16,18 +16,54 @@
+class qrcodeController extends Controller
+{
+
+    public function qrCode(Request $request){
+        $data=DB::table('users')->where('id','=',$request->input('user_id'))->get();
+    }
+    public function addqrCode(Request $request){
+      $userdetails=Auth::user(); 
+     // dd($userdetails->id);
+        $data=DB::table('book_parking')
+        ->join('users','book_parking.user_id','=','users.id')
+        ->join('payments_status','book_parking.payment_status','payments_status.id')
+        ->where('book_parking.id','=',$userdetails->id)
+
+
+        ->select('users.id','users.name','book_parking.id','book_parking.paking_type','book_parking.parking_amt','book_parking.parking_slot_number','book_parking.start_date','book_parking.end_date','book_parking.payment_status','payments_status.paysta_status_name')->get();
+
+       $d=QrCode::generate($data,public_path('/images'));
+       return response()->json( ["qr_image_url"=>'/images'],200);
+        $files=QrCode::size(100)->generate($data);
+         //$qrcode=env('APP_URL').'/images'.'/qrcode'.'/'.$files; 
+
+        //  $userdetails=Auth::user(); 
+         //dd($userdetails->id);
+
+         $userdetails=new BookParkingModel;
+         $userdetails->paking_type=$request->input('paking_type');
+         $userdetails->parking_amt=$request->input('parking_amt');
+         $userdetails->user_id =(int)$userdetails->id;
+         $userdetails->address_id =$request->input('address_id');
+         $userdetails->payment_status =$request->input('payment_status');
+         $userdetails->parking_status =$request->input('parking_status');
+         $userdetails->payment_id =$request->input('payment_id');
+         $userdetails->start_date =$request->input('start_date');
+         $userdetails->	end_date =$request->input('	end_date');
+         $userdetails->parking_slot_number =$request->input('parking_slot_number');
+         $userdetails->qrcode=$files;
+         $userdetails-> save();
+         return response()->json(['status'=>'Sucess','message'=>'Deatils uploaded sucessfully'],200);
+
+
+     //   $d=QrCode::generate($data,public_path('/images'));
+
+return $qrcode;
+
+}
+
+       // return response()->json( ["qr_image_url"=>'/images'],200);
+public function getqrCode(Request $request){
+
+  $userdetails=Auth::user(); 
+  $data=DB::table('book_parking')
+  ->join('users','book_parking.user_id','=','users.id')
+  ->join('payments_status','book_parking.payment_status','payments_status.id')
+  ->where('book_parking.user_id','=',$userdetails->id)
+
+
+  ->select('users.id','users.name','book_parking.id','book_parking.paking_type','book_parking.parking_amt','book_parking.parking_slot_number','book_parking.start_date','book_parking.end_date','book_parking.payment_status','payments_status.paysta_status_name','book_parking.qrcode')->get();
+
+  return $data;
+}
+}
