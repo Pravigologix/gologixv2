3875bfc313039151b37520ef378e48aa533b42c2
naveenramachandru <61786249+naveenramachandru@users.noreply.github.com>
2022-10-07 15:17:59 +0530

    Update ParkingController.php

--- app/Http/Controllers/API/User/cloudparking/ParkingController.php
+++ app/Http/Controllers/API/User/cloudparking/ParkingController.php
@@ -8,13 +8,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use DB;
 
-//class ParkingController extends Controller {
- //   public function index() {
 
-  //      $userdetails = Auth::user();
-
-//<<<<<<< ashwini
 class ParkingController extends Controller
 {
+    
+    
+    
    
 }
