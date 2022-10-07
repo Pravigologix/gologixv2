2cb0498b010bf0d8c8f58e9fde51ea3567c3d378
naveenramachandru <61786249+naveenramachandru@users.noreply.github.com>
2022-10-07 15:51:31 +0530

    Update ParkingController.php

--- app/Http/Controllers/API/User/cloudparking/ParkingController.php
+++ app/Http/Controllers/API/User/cloudparking/ParkingController.php
@@ -13,7 +13,15 @@ class ParkingController extends Controller
 {
      public function getalladdress( Request $request ) {
        
-        $data = DB::table( 'address' )
+        $data = DB::table( 'addresses' )
+            ->leftjoin('add_parking_slots','addresses.id','=','add_parking_slots.address_id')
+             ->leftjoin('parking_charges','add_parking_slots.id','=','parking_charges.add_praking_slot_id')
+            ->where('parking_charges.is_active','0')
+            ->where('addresses.add_isactive','0')->select(
+            'addresses.add_description','addresses.add_address','addresses.add_city_id','addresses.add_pincode','addresses.add_latitude','addresses.longitude',
+            'add_parking_slots.id as slot_id','add_parking_slots.parking_type','add_parking_slots.parking_no','add_parking_slots.starts_at','add_parking_slots.ends_at','add_parking_slots.parking_slots',
+        'parking_charges.*'
+        )
         
         
       
