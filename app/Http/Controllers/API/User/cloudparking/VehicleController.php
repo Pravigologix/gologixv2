033da1f4f01d2620221ccadabcb8273159b9e705
naveenramachandru <61786249+naveenramachandru@users.noreply.github.com>
2022-10-09 17:24:49 +0530

    Update VehicleController.php

--- app/Http/Controllers/API/User/cloudparking/VehicleController.php
+++ app/Http/Controllers/API/User/cloudparking/VehicleController.php
@@ -29,13 +29,12 @@ class VehicleController extends Controller
       public function editVehicle(Request $request){
 
         $userdetails= Auth::user();  
-        //dd($userdetails->id);
+       
 
     $data=DB::table('user_vehicle')->where('id','=',$request->input('id'))
         ->where('user_vehicle.useveh_user_id','=',$userdetails->id)->update(['useveh_vehicle_name'=>$request->input('useveh_vehicle_name'),'useveh_vehicle_number'=>$request->input('useveh_vehicle_number')]);
       
-    //$d=DB::table('banners')->get();
-    // return $d;
+   
       return response()->json(['data successfully updated.']);
   }
   
