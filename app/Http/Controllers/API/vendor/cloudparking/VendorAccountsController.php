a59ac2da75df03d6c697bf84f91fed6c23ad5534
tejashwinirm <111041715+tejashwinirm@users.noreply.github.com>
2022-10-12 11:59:00 +0530

    vendor accounts api updated

--- app/Http/Controllers/API/vendor/cloudparking/VendorAccountsController.php
+++ app/Http/Controllers/API/vendor/cloudparking/VendorAccountsController.php
@@ -14,31 +14,11 @@ class VendorAccountsController extends Controller
 
     public function addVendorAccountDetails(Request $request){
 
-
-  /*
-        //if($user-> is_admin==2){
-        $payment_status=DB::table('payments_status')->insert( [
-            'paysta_status_name'=>$request->input( 'paysta_status_name' ),
-            'paysta_description'=>$request->input( 'paysta_description' ),
-            'paysta_isactive'=>$request->input( 'paysta_isactive' ),
-            'paysta_isdeleted'=>$request->input( 'paysta_isdeleted' ),
-            'user_id'=>$user->id,
-        ] );
-        if($payment_status){
-            $payment_status_id=DB::table('payments_status')
-            ->where('payments_status.user_id','=',$user->id)->first('id');
-    */
-  
-
- 
-           
-
-          // dd($paymentid);
        
         $user= Auth::user();  
         //dd($user->id);
             $userdetails=new VendorAccounts;
-            $userdetails->vendor_id=$request->input('vendor_id');
+            $userdetails->vendor_id=$user->id;
             $userdetails->venacc_name=$request->input('venacc_name');
             $userdetails->venacc_alias=$request->input('venacc_alias');
             $userdetails->venacc_bank_name=$request->input('venacc_bank_name');
@@ -58,9 +38,8 @@ class VendorAccountsController extends Controller
 
 //else{
  //   return response()->json(['your not vendor']);
-//}
-   // }
-    /*
+
+   
     public function editVendorAccountDetails(Request $request){
 
         $userdetails= Auth::user();  
@@ -68,14 +47,13 @@ class VendorAccountsController extends Controller
 
     $data=DB::table('vendor_account')
         ->where('vendor_account.vendor_id','=',$userdetails->id)
-         ->where('vendor_account.id','=',$request->input('id'))
-        ->update(['venacc_name'=>$request->input('venacc_name'),'venacc_bank_name'=>$request->input('venacc_bank_name'),'venacc_account_no'=>$request->input('venacc_account_no'),'venacc_ifsc'=>$request->input('venacc_ifsc')]);
+        ->update(['venacc_name'=>$request->input('venacc_name'),'venacc_alias'=>$request->input('venacc_alias'),'venacc_ifsc'=>$request->input('venacc_ifsc'),'venacc_bank_name'=>$request->input('venacc_bank_name'),'venacc_account_no'=>$request->input('venacc_account_no'),'venacc_ifsc'=>$request->input('venacc_ifsc')]);
       
    
       return response()->json(['data successfully updated.']);
   }
 
-*/
+
 public function getVendorAccountDetails(Request $request)
 {  
 
