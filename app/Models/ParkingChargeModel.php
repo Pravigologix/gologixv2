08e62de2f014f8da91144961b643d839e00b9c68
naveenramachandru <61786249+naveenramachandru@users.noreply.github.com>
2022-10-15 11:55:44 +0530

    Update ParkingChargeModel.php

--- app/Models/ParkingChargeModel.php
+++ app/Models/ParkingChargeModel.php
@@ -14,13 +14,13 @@ class ParkingChargeModel extends Model
 
 
     public function add_praking_desc(){
-        return $this->belongsToMany(ParkingDescriptionModel::class);
+        return $this->belongsTo(ParkingDescriptionModel::class);
     }
 
     public function add_desc(){
         return $this->belongsToMany(AddressModel::class);
     }
     public function add_praking_slot(){
-        return $this->belongsToMany(ParkingSlotModel::class);
+        return $this->belongsTo(ParkingSlotModel::class);
     }
 }
