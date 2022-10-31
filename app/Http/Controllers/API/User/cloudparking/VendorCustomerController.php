8247afd1cb02d1eddd611ff6a62ba86369fe5835
naveenramachandru <61786249+naveenramachandru@users.noreply.github.com>
2022-10-31 12:52:07 +0530

    Update VendorCustomerController.php

--- app/Http/Controllers/API/User/cloudparking/VendorCustomerController.php
+++ app/Http/Controllers/API/User/cloudparking/VendorCustomerController.php
@@ -38,21 +38,27 @@ class VendorCustomerController extends Controller
         ]);
     }
     }
-    public function vendor(Request $request){
+     public function vendor(Request $request){
         $user = Auth::user();
         //dd($user->id);
         if($user!=null){
-            $data=DB::table('book_parking')
-            ->leftjoin('addresses','book_parking.address_id','=','addresses.id')
-            ->leftjoin('users','book_parking.user_id','=','users.id')
-            ->leftjoin('user_vehicle','user_vehicle.useveh_user_id','=','users.id')
-            //->where('users.id','=','addresses.add_user_id')
-            ->where('users.is_admin','=',2)
-            ->where('addresses.add_user_id','=',$user->id)
-            ->select('users.name','users.id','users.phonenumber','email','add_city_id','add_pincode','add_description','is_admin','user_vehicle.useveh_vehicle_name','user_vehicle.useveh_vehicle_number','book_parking.parking_status')
-            // 
+            $data=BookParkingModel::
+            with('address_details')->
+            whereHas('address_details',function($qurey){
+                // $qurey->where('is_cloud_parking',1);
+                $qurey->where('add_user_id',Auth::user()->id);
+
+            })
+           -> with('user_details')
+           ->with('parking_charge_details')
+           ->with('all_parking_charge_details')
+
+           ->with('parking_slot_address_details')
+
+
+        
           
-            ->paginate(30);
+            ->paginate(80);
             return response()->json(["user_details_for_vendor"=>$data],200);
         }
         else{
