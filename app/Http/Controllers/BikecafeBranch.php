09ca847558987bfc5ea236b1fdf6472b8f280973
Naveen B R <naveenramachandru77@gmail.com>
2022-11-18 11:02:50 +0530

    api update

--- app/Http/Controllers/BikecafeBranch.php
+++ app/Http/Controllers/BikecafeBranch.php
@@ -9,8 +9,8 @@ class BikecafeBranch extends Controller
 {
     public function getallbcbranch(Request $req)
     {
-        $bc_cafe=DB::table('bc_branch')->where('bc_type',0)->where('isactive',1)->get();
-        $bc_ev=DB::table('bc_branch')->where('bc_type',1)->where('isactive',1)->get();
+        $bc_cafe=DB::table('bc_branch')->where('bc_type',0)->get();
+        $bc_ev=DB::table('bc_branch')->where('bc_type',1)->get();
 
         return response()->json([
             "BC Cafe"=>$bc_cafe,
