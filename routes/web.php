c4bb77478f1949697ceb1967834d4c31fd9724c8
Naveen B R <naveenramachandru77@gmail.com>
2022-11-23 12:25:45 +0530

    api updated

--- routes/web.php
+++ routes/web.php
@@ -37,7 +37,7 @@ Route::get('routes', function () {
 
 
 
-Route::group(['domain' =>'admin.bikecafe.co.in'])->group(function () {
+
     Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 Route::get('/', [AdminController::class, 'login'])->name('admin-login');
@@ -157,7 +157,7 @@ Route::get('/banner',function(){
         });
   
    
-});
+
 
 
 
